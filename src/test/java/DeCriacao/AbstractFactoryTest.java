fc0b79b749e8661d621b08b831207f19c197a2e4
VitorDie <vitordie@protonmail.ch>
2024-03-17 16:58:39 -0300

    O padrão Abstract Factory foi incluido no projeto; agora em test, existe código de exemplo, e por fim, tirei o main(), porque não vou usar nesse projeto. Se quiser ver código, vá para os testes!

--- src/test/java/DeCriacao/AbstractFactoryTest.java
+++ src/test/java/DeCriacao/AbstractFactoryTest.java
@@ -1,11 +1,16 @@
 package DeCriacao;
 
+import org.junit.Assert;
 import org.junit.Test;
 import padroesDeCriacao.abstractFactory.mundoInteligivel.AbstractFactory;
 import padroesDeCriacao.abstractFactory.mundoInteligivel.products.AbstractProductA;
 import padroesDeCriacao.abstractFactory.mundoInteligivel.products.AbstractProductB;
 import padroesDeCriacao.abstractFactory.mundoSensivel.ConcreteFactory1;
 import padroesDeCriacao.abstractFactory.mundoSensivel.ConcreteFactory2;
+import padroesDeCriacao.abstractFactory.mundoSensivel.products.ProductA1;
+import padroesDeCriacao.abstractFactory.mundoSensivel.products.ProductA2;
+import padroesDeCriacao.abstractFactory.mundoSensivel.products.ProductB1;
+import padroesDeCriacao.abstractFactory.mundoSensivel.products.ProductB2;
 
 public class AbstractFactoryTest {
     @Test
@@ -13,11 +18,17 @@ public class AbstractFactoryTest {
         AbstractFactory factory = new ConcreteFactory1();
         AbstractProductA pa = factory.createProductA();
         AbstractProductB pb = factory.createProductB();
+
+        Assert.assertTrue(pa instanceof ProductA1);
+        Assert.assertTrue(pb instanceof ProductB1);
     }
 
     public void fabrica2Test() {
         AbstractFactory factory = new ConcreteFactory2();
         AbstractProductA pa = factory.createProductA();
         AbstractProductB pb = factory.createProductB();
+
+        Assert.assertTrue(pa instanceof ProductA2);
+        Assert.assertTrue(pb instanceof ProductB2);
     }
 }
